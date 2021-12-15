3d059163357fb07f312c9471a5233d32a4a9b050
Muhammadjon226 <valiyevmuhammadjon@gmail.com>
2021-12-15 19:45:25 +0500

    Fix Bugs

--- link.go
+++ link.go
@@ -11,7 +11,7 @@ import (
 
 func SendLink (data CommentInfo) error {
 
-	
+	fmt.Println("SendLinkga keldi")
 	Sender := "gobootcamp075@gmail.com"
 	Password := "1501069Mm"
 
@@ -30,6 +30,7 @@ func SendLink (data CommentInfo) error {
 		log.Fatal(err)
 		return err
 	}
+
 	var body bytes.Buffer
 	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 	body.Write([]byte(fmt.Sprintf("Subject: This is a test \n%s\n\n", mimeHeaders)))
